7101955a4f662c423ca23bdbf242b5ea51b03bb1
Dorin Manase <dmanase@gmail.com>
2016-02-09 12:00:47 -0500

    Updated user queries and format changes

--- server/views.py
+++ server/views.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 from server import app
 import os
 import glob
@@ -7,7 +5,7 @@ import sqlite3
 import hashlib
 import gzip
 import json
-from flask import jsonify, make_response, request, g, abort
+from flask import jsonify, make_response, request, g
 from contextlib import closing
 
 
@@ -18,7 +16,7 @@ def connect_db():
 
 def init_db():
     # initalize the database if it doesn't already exist.
-    # this step needs to be run manually form the command line
+    # this step needs to be run manually from the command line
     # from server import views; views.init_db()
     with closing(connect_db()) as db:
         with open(app.config['SCHEMA'], mode='r') as f:
@@ -86,7 +84,7 @@ def merge_chunks(file_chunks, output_name):
 
 
 @app.route("/upload", methods=['GET'])
-def transfer_info():
+def resumable_info():
     identifier = request.args.get('resumableIdentifier', type=str)
     filename = request.args.get('resumableFilename', type=str)
     chunk_number = request.args.get('resumableChunkNumber', type=int)
@@ -109,7 +107,7 @@ def resumable_upload():
     total_chunks = request.form.get('resumableTotalChunks', type=int)
     chunk_size = request.form.get('resumableChunkSize', type=int)
     total_size = request.form.get('resumableTotalSize', type=int)
-    identifier = request.form.get('resumableIdentifier', type=str)
+    identifier = request.form.get('resumableIdentifier', type=str)  # assumes this is unique per file not chunk!
     filename = request.form.get('resumableFilename', type=str)
 
     # Check for missing or invalid parameters
@@ -135,39 +133,24 @@ def resumable_upload():
 
     if len(all_chunks) == int(total_chunks):
         merge_chunks(all_chunks, filename)
+        # TODO integrity check (md5, .gz/.bam)
+        # add file to database TODO associate file with user/owner and include file metadata
+        g.db.execute('insert into files (filename) values ("%s")' % filename)
+        g.db.commit()
         return make_response(jsonify({'Download complete': 'Successfully received file'}), 200)
 
     return make_response(jsonify({'Download complete': 'Successfully received chunk'}), 200)
 
-    # if file_complete:
-    #     file_type = base_filename.rsplit('.', 1)[1].lower()
-    #     if file_type == 'bam':
-    #         passed_integrity_check = bam_test(os.path.join(app.config['UPLOAD_FOLDER'], base_filename))
-    #     else:  # file is therefore *.gz
-    #         passed_integrity_check = gzip_test(os.path.join(app.config['UPLOAD_FOLDER'], base_filename))
-    #
-    #     passed_md5_check = md5_test(header['X-MD5-Checksum'], os.path.join(app.config['UPLOAD_FOLDER'], base_filename))
-    #
-    #     if passed_md5_check and passed_integrity_check:
-    #         return make_response(jsonify({'Download complete': 'Successfully received file'}))
-    #     else:
-    #         return make_response(jsonify({'Download complete': 'ERROR, the received file is corrupt or incomplete!'}))
-    #
-    #         # TODO: update database with file and owner
-
 
 @app.route('/users', methods=['GET'])
 def show_users():
-    cur = g.db.execute('select * from users order by user_id asc')
-    users = cur.fetchall()
-    return make_response(str(users))
+    users = g.db.execute('select * from users order by user_id asc').fetchall()
+    return make_response(jsonify({t[0]:t[1:] for t in users}))
 
 
-@app.route('/user/<username>')
+@app.route('/user/<username>', methods=['GET'])
 def show_user_files(username):
-    # query db to check for appropriate auth-token
-    # if matched, return user_ID and query for a list of all their files
-    # show the user their available files
-    return 'User %s' % username
-
+    # query db to check for appropriate auth-token first?
+    user_files = g.db.execute('select * from files where owner = "%s"' % username).fetchall()
+    return make_response(jsonify({t[0]:t[1:] for t in user_files}))
 
